9e79922ad8451f7a5f96809f82196a37ef3fb727
JM <minaj002@gmail.com>
2019-07-22 10:53:15 +0300

    clean up

--- src/main/java/com/ubtn/RestApplication.java
+++ src/main/java/com/ubtn/RestApplication.java
@@ -2,11 +2,16 @@ package com.ubtn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@RestController
-public class RestApplication {
+public class RestApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
